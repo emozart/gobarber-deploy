61205951cba59b3e5a7ce3c7008b8db5702aa0af
Elton Mozart <elton.mozart@gmail.com>
2020-05-01 20:21:27 -0300

    Inclusão da rota de criação de usuários do sistema

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
 import appointmentRouter from './appointmens.routes'
+import userRouter from './users.routes'
 
 const routes = Router()
 
 routes.use('/appointments', appointmentRouter)
+routes.use('/users', userRouter)
 
 export default routes
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,36 @@
+import { getRepository } from 'typeorm'
+import { hash } from 'bcryptjs'
+
+import User from '../models/User'
+
+interface CreateUserRequest {
+  name: string
+  email: string
+  password: string
+}
+
+class CreateUserService {
+  async axecute({ name, email, password }: CreateUserRequest): Promise<User> {
+    const usersRepository = getRepository(User)
+
+    const checkUserExists = await usersRepository.findOne({ where: { email } })
+
+    if (checkUserExists) {
+      throw new Error('This email is already in use.')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword
+    })
+
+    await usersRepository.save(user)
+
+    return user
+  }
+}
+
+export default CreateUserService
